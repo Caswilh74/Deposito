d789c50f5a3f3062e54105972b7aadda6f00c63e
Gustavo Carvalho <gcarvalh@c2r1s2.42luxembourg.lu>
2024-07-28 14:07:23 +0200

    C04

--- C/C04/ex03/ft_atoi.c
+++ C/C04/ex03/ft_atoi.c
@@ -6,8 +6,42 @@
 /*   By: gcarvalh <gcarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/27 16:04:25 by gcarvalh          #+#    #+#             */
-/*   Updated: 2024/07/27 16:04:41 by gcarvalh         ###   ########.fr       */
+/*   Updated: 2024/07/28 13:58:40 by gcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+
+int		ft_atoi(char *str)
+{
+	int		i;
+	int		sign;
+	int		result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (str[i] && ((str[i] >= 9 && str[i] <= 13) || str[i] == 32))
+			i++;
+	while (str[i] && (str[i] == '+' || str[i] == '-'))
+	{
+			if (str[i] == '-')
+			sign *= -1;
+			i++;	
+	}	
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	{
+			result = (str[i] - '0') + (result * 10);
+			i++;
+	}
+	return (result * sign);
+}
+/*
+int	main(void)
+{
+	int x;
+	
+	x = ft_atoi("   ---+--+1234ab567");
+	printf("%d", x);
+}
+*/
