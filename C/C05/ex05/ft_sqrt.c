d388b67e0cd541512a487b51436ad9d2a54dc208
Gustavo Carvalho <gcarvalh@c2r1s2.42luxembourg.lu>
2024-07-30 15:03:04 +0200

    done

--- C/C05/ex05/ft_sqrt.c
+++ C/C05/ex05/ft_sqrt.c
@@ -6,7 +6,7 @@
 /*   By: gcarvalh <gcarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 17:12:32 by gcarvalh          #+#    #+#             */
-/*   Updated: 2024/07/30 13:25:18 by gcarvalh         ###   ########.fr       */
+/*   Updated: 2024/07/30 15:02:25 by gcarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,16 @@
 
 int	ft_sqrt(int nb)
 {
-	int	result;
+	long int	result;
 
-	result = 0;
-	if (nb < 0)
+	result = 1;
+	if (nb <= 0)
 		return (0);
 	while (result * result < nb)
 		result++;
-	if (result * result == nb)
+	if (result * result  == nb)
 		return (result);
-	else
-		return (0);
+	return (0);
 }
 /*
 int	main(void)
